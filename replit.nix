8dadc78ae401950f6b8db71cfb193fe21af9d5a8
kollarsandor <budoskurva56@gmail.com>
2026-05-02 22:55:13 +0200

    Expand dependencies in replit.nix
    Added multiple dependencies including Python, LLVM, and various build tools.

--- replit.nix
+++ replit.nix
@@ -1,5 +1,25 @@
-{pkgs}: {
-  deps = [
-    pkgs.zig
-  ];
+{pkgs}: {  
+  deps = [  
+    pkgs.zig  
+    pkgs.python3  
+    pkgs.python3Packages.pip  
+    pkgs.futhark  
+    pkgs.z3  
+    pkgs.llvm_17  
+    pkgs.clang_17  
+    pkgs.lld_17  
+    pkgs.gcc  
+    pkgs.gnumake  
+    pkgs.curl  
+    pkgs.gnutar  
+    pkgs.gzip  
+    pkgs.unzip  
+    pkgs.yosys  
+    pkgs.nextpnr  
+    pkgs.icestorm  
+    pkgs.verilator  
+    pkgs.nodejs  
+    pkgs.bash  
+    pkgs.pkg-config  
+  ];  
 }
